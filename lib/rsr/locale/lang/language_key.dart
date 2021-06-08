69c5fe3f48f64379b43db420791b46224b6aa889
michael andom <michaelandom03@gmail.com>
2021-06-08 14:32:15 +0300

    coupon done

--- lib/rsr/locale/lang/language_key.dart
+++ lib/rsr/locale/lang/language_key.dart
@@ -1,4 +1,23 @@
 class LanguageKey {
   static const String AMHARIC_LC = "et_am";
   static const String ENGLISH_LC = "en";
+
+  static const String HOME = "HOME";
+  static const String NEWS = "NEWS";
+  static const String SHOP = "SHOP";
+  static const String WALLET = "WALLET";
+  static const String WISH_LIST = "WISH_LIST";
+  static const String ORDERS = "ORDERS";
+  static const String CART = "CART";
+  static const String BEST_SELLERS = "BEST_SELLERS";
+  static const String SETTINGS = "SETTINGS";
+  static const String CONTACT_US = "CONTACT_US";
+  static const String SHARE = "SHARE";
+  static const String HELP = "HELP";
+  static const String SING_OUT = "SING_OUT";
+  static const String SING_IN = "SING_IN";
+  static const String CHANGE_THEME = "CHANGE_THEME";
+  static const String YES = "YES";
+  static const String NO = "NO";
+  static const String ERROR = "ERROR";
 }
